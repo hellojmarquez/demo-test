11e6db84dc1def5a3a73d4cdea556455f36bf4db
Javier Marquez <hello.jmarquez@gmail.com>
2025-04-28 16:28:05 -0500

    fix error mongo cached

--- src/lib/db.ts
+++ src/lib/db.ts
@@ -8,13 +8,25 @@ if (!MONGODB_URI) {
 	);
 }
 
+interface MongooseCache {
+	conn: typeof mongoose | null;
+	promise: Promise<typeof mongoose> | null;
+}
+
+declare global {
+	var mongoose: MongooseCache | undefined;
+}
+
 let cached = global.mongoose;
 
 if (!cached) {
 	cached = global.mongoose = { conn: null, promise: null };
 }
-
 export async function connectToDatabase() {
+	if (!cached) {
+		cached = global.mongoose = { conn: null, promise: null };
+	}
+
 	if (cached.conn) {
 		return cached.conn;
 	}

3f94b0a14b6eec14dd52f92e3d0663938fbbe5de
Javier Marquez <hello.jmarquez@gmail.com>
2025-05-23 01:53:28 -0500

    add boton distribuir

---
+++ src/app/api/admin/distributeRelease/[id]/route.ts
@@ -0,0 +1,84 @@
+// app/api/admin/updateRelease/[id]/route.ts
+import { NextResponse, NextRequest } from 'next/server';
+import Release from '@/models/ReleaseModel';
+
+import { jwtVerify } from 'jose';
+
+export async function POST(
+	req: NextRequest,
+	{ params }: { params: { id: string } }
+) {
+	try {
+		const moveMusicAccessToken = req.cookies.get('accessToken')?.value;
+		const token = req.cookies.get('loginToken')?.value;
+		if (!token) {
+			return NextResponse.json(
+				{ success: false, error: 'Not authenticated' },
+				{ status: 401 }
+			);
+		}
+
+		// Verificar JWT
+		try {
+			const { payload: verifiedPayload } = await jwtVerify(
+				token,
+				new TextEncoder().encode(process.env.JWT_SECRET)
+			);
+		} catch (err) {
+			console.error('JWT verification failed', err);
+			return NextResponse.json(
+				{ success: false, error: 'Invalid token' },
+				{ status: 401 }
+			);
+		}
+		const externalId = params.id;
+		const release = await Release.findOne({ external_id: externalId });
+		console.log('release', release);
+		if (!release) {
+			return NextResponse.json(
+				{ success: false, error: 'Release not found' },
+				{ status: 404 }
+			);
+		}
+		const distributeReq = await fetch(
+			`${process.env.MOVEMUSIC_API}/releases/update-status/`,
+			{
+				method: 'POST',
+				headers: {
+					'Content-Type': 'application/json',
+					Authorization: `JWT ${moveMusicAccessToken}`,
+					'x-api-key': process.env.MOVEMUSIC_X_APY_KEY || '',
+					Referer: process.env.MOVEMUSIC_REFERER || '',
+				},
+				body: JSON.stringify({
+					id: externalId,
+					action: 'distribute',
+				}),
+			}
+		);
+
+		const distributeRes = await distributeReq.json();
+		if (distributeRes.error) {
+			return NextResponse.json(
+				{
+					success: false,
+					message: distributeRes.message,
+				},
+				{ status: 400 }
+			);
+		}
+		return NextResponse.json({
+			success: true,
+			message: 'Release en revisión',
+		});
+	} catch (error: any) {
+		console.error('Error al distribuir el release:', error);
+		return NextResponse.json(
+			{
+				success: false,
+				message: error.message || 'Error al actualizar el release',
+			},
+			{ status: 500 }
+		);
+	}
+}
--- src/components/UpdateReleaseModal.tsx
+++ src/components/UpdateReleaseModal.tsx
@@ -516,16 +516,68 @@ const UpdateReleasePage: React.FC<UpdateReleasePageProps> = ({
 			toast.error('Error al cargar los datos del track');
 		}
 	};
-
+	const handleDistribute = async () => {
+		setIsLoading(true);
+		try {
+			const response = await fetch(
+				`/api/admin/distributeRelease/${safeFormData.external_id}`,
+				{
+					method: 'POST',
+					body: JSON.stringify(safeFormData),
+				}
+			);
+			const data = await response.json();
+			console.log('data', data);
+			if (data.success) {
+				toast.success(data.message);
+			} else {
+				toast.error(data.message);
+			}
+		} catch (error) {
+			console.error('Error:', error);
+			toast.error(`Error: ${error}`);
+		} finally {
+			setIsLoading(false);
+		}
+	};
 	return (
 		<div className="container mx-auto md:px-4 py-8">
 			<audio ref={audioRef} />
 			<div className="flex justify-end">
 				<button
-					onClick={() => {}}
-					className="bg-black text-white font-bold px-6 py-2"
+					onClick={handleDistribute}
+					disabled={isLoading}
+					className={`bg-black text-white font-bold px-6 py-2 flex items-center justify-center min-w-[120px] ${
+						isLoading ? 'opacity-70 cursor-not-allowed' : ''
+					}`}
 				>
-					Distribuir
+					{isLoading ? (
+						<>
+							<svg
+								className="animate-spin -ml-1 mr-3 h-5 w-5 text-white"
+								xmlns="http://www.w3.org/2000/svg"
+								fill="none"
+								viewBox="0 0 24 24"
+							>
+								<circle
+									className="opacity-25"
+									cx="12"
+									cy="12"
+									r="10"
+									stroke="currentColor"
+									strokeWidth="4"
+								></circle>
+								<path
+									className="opacity-75"
+									fill="currentColor"
+									d="M4 12a8 8 0 018-8V0C5.373 0 0 5.373 0 12h4zm2 5.291A7.962 7.962 0 014 12H0c0 3.042 1.135 5.824 3 7.938l3-2.647z"
+								></path>
+							</svg>
+							Procesando...
+						</>
+					) : (
+						'Distribuir'
+					)}
 				</button>
 			</div>
 			<div className="bg-white rounded-lg md:p-6">

0025f585a892019ba98ddf94f6f3d740bd7d8959
Javier Marquez <hello.jmarquez@gmail.com>
2025-06-18 08:09:33 -0500

    http 0000

--- src/server.ts
+++ src/server.ts
@@ -16,8 +16,9 @@ app.prepare().then(() => {
 
 	// Inicializar Socket.IO
 	initSocket({ socket: { server } } as any);
+	const PORT = parseInt(process.env.PORT || '3000', 10);
 
-	server.listen(PORT, () => {
-		console.log(`> Ready on http://localhost:${PORT}`);
+	server.listen(PORT, '0.0.0.0', () => {
+		console.log(`Ready on http://0.0.0.0:${PORT}`);
 	});
 });
